849cd99c2933a44417cba3e9c5936c2507961231
machine_x3000 <palatenco@yandex.ru>
2022-08-23 21:09:50 +0300

    my_project(v1)

---
+++ .vscode/guess the number.py
@@ -0,0 +1,57 @@
+"""Компьютер загадывает целое число от 1 до 100, 
+и нам его нужно угадать.
+Под «угадать» 
+подразумевается «написать программу, которая угадывает число»."""
+
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    
+    number = np.random.randint(1, 101) # Загадываем число
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
+    count = 0
+    predict_number = np.random.randint(1, 101) #Предпологаемое число
+    
+    while True:
+        count += 1
+        if number < predict_number:
+            predict_number = np.random.randint(1, predict_number) #Если предпологаемое больше загаданного
+
+        if number > predict_number:
+            predict_number = np.random.randint(predict_number, 101) #Если предпологаемое меньше загаданного
+            
+        if number == predict_number:
+            break 
+
+        
+    return(count) # возвращаем кол-во попыток
+
+def score_game(random_predict) -> int: 
+    """За какое кол-во попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict (_type_): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1) #фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size =(1000)) # загадали список чисел
+    
+    for number in random_array:
+        count_ls.append(random_predict(number))
+        
+    score = int(np.mean(count_ls))
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return (score)
+
+print(score_game(random_predict))
